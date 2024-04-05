9ce782061a5824b6bd6e8434a7dab55168c28515
Fergus Molloy <fergus@molloy.xyz>
2024-04-05 23:57:16 +0100

    Add some seed data

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,33 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+%{:id => project_id} =
+  Cardz.Repo.insert!(%Cardz.Projects.Project{
+    title: "Cardz",
+    description: "Demo project",
+    abbr: "cdz"
+  })
+
+%{:id => todo_id} =
+  Cardz.Repo.insert!(%Cardz.Columns.Column{
+    title: "Todo",
+    project_id: project_id
+  })
+
+%{:id => in_progress_id} =
+  Cardz.Repo.insert!(%Cardz.Columns.Column{
+    title: "In Progress",
+    project_id: project_id
+  })
+
+%{:id => done_id} =
+  Cardz.Repo.insert!(%Cardz.Columns.Column{
+    title: "Done",
+    project_id: project_id
+  })
+
+Cardz.Repo.insert!(%Cardz.Cards.Card{title: "Create a new card", column_id: todo_id})
+Cardz.Repo.insert!(%Cardz.Cards.Card{title: "Create a new column", column_id: todo_id})
+Cardz.Repo.insert!(%Cardz.Cards.Card{title: "Explore cardz app", column_id: in_progress_id})
+Cardz.Repo.insert!(%Cardz.Cards.Card{title: "Setup cardz", column_id: done_id})
