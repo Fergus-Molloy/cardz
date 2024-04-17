fc97714b8180bfbe233fdba0746d9f2483459c40
Fergus Molloy <fergus@molloy.xyz>
2024-04-17 16:16:10 +0100

    Can delete cards
    closes #5

--- lib/cardz_web/components/card/form_component.ex
+++ lib/cardz_web/components/card/form_component.ex
@@ -8,10 +8,22 @@ defmodule CardzWeb.Components.Card.FormComponent do
   def render(assigns) do
     ~H"""
     <div>
-      <.header>
-        <%= @title %>
-        <:subtitle>Use this form to manage project records in your database.</:subtitle>
-      </.header>
+      <div>
+        <.header>
+          <%= @title %>
+          <:subtitle>Use this form to manage project records in your database.</:subtitle>
+        </.header>
+        <.simple_form for={@form} id="delete-card" phx-target={@myself} phx-submit="delete_card">
+          <div class="hidden">
+            <.input field={@form[:id]} type="number" value={@card.id} />
+          </div>
+          <:actions>
+            <.button value="test" data-confirm="Are you sure?" phx-disable-with="Deleting...">
+              Delete
+            </.button>
+          </:actions>
+        </.simple_form>
+      </div>
 
       <.simple_form
         for={@form}
@@ -50,15 +62,31 @@ defmodule CardzWeb.Components.Card.FormComponent do
     {:noreply, assign_form(socket, changeset)}
   end
 
+  @impl true
   def handle_event("save", %{"card" => card_params}, socket) do
     save_card(socket, socket.assigns.action, card_params)
   end
 
+  @impl true
+  def handle_event("delete_card", %{"card" => card_params}, socket) do
+    %{"id" => id} = card_params
+    card = Cards.get_card!(id)
+
+    case Cards.delete_card(card) do
+      {:ok, _card} ->
+        {:noreply,
+         socket
+         |> put_flash(:info, "card deleted successfully")
+         |> push_patch(to: socket.assigns.patch)}
+
+      {:error, _} ->
+        {:noreply, socket |> put_flash(:error, "could not delete card")}
+    end
+  end
+
   defp save_card(socket, :edit_card, card_params) do
     case Cards.update_card(socket.assigns.card, card_params) do
-      {:ok, card} ->
-        notify_parent({:saved_card, card})
-
+      {:ok, _card} ->
         {:noreply,
          socket
          |> put_flash(:info, "card updated successfully")
@@ -76,8 +104,6 @@ defmodule CardzWeb.Components.Card.FormComponent do
     |> Cards.create_card(socket.assigns.column_id, project.id)
     |> case do
       {:ok, _card} ->
-        notify_parent({:saved_card, project})
-
         {:noreply,
          socket
          |> put_flash(:info, "card created successfully")
@@ -92,5 +118,5 @@ defmodule CardzWeb.Components.Card.FormComponent do
     assign(socket, :form, to_form(changeset))
   end
 
-  defp notify_parent(msg), do: send(self(), {__MODULE__, msg})
+  # defp notify_parent(msg), do: send(self(), {__MODULE__, msg})
 end
--- lib/cardz_web/live/project_live/show.ex
+++ lib/cardz_web/live/project_live/show.ex
@@ -18,56 +18,60 @@ defmodule CardzWeb.ProjectLive.Show do
   defp apply_action(socket, :show, %{"id" => id}) do
     socket
     |> assign(:page_title, page_title(socket.assigns.live_action))
-    |> assign(:project, Projects.get_project!(id))
+    |> assign_project(id)
   end
 
   defp apply_action(socket, :edit, %{"id" => id}) do
     socket
     |> assign(:page_title, page_title(socket.assigns.live_action))
-    |> assign(:project, Projects.get_project!(id))
+    |> assign_project(id)
   end
 
   defp apply_action(socket, :new_column, %{"id" => project_id}) do
     socket
     |> assign(:page_title, "New Column")
     |> assign(:column, %Columns.Column{project_id: project_id})
-    |> assign(:project, Projects.get_project!(project_id))
+    |> assign_project(project_id)
   end
 
   defp apply_action(socket, :edit_column, %{"id" => project_id, "column_id" => column_id}) do
     socket
     |> assign(:page_title, "Edit Column")
     |> assign(:column, Columns.get_column!(column_id))
-    |> assign(:project, Projects.get_project!(project_id))
+    |> assign_project(project_id)
   end
 
   defp apply_action(socket, :new_card, %{"id" => project_id, "column_id" => column_id}) do
     socket
-    |> assign(:column_id, column_id)
     |> assign(:page_title, "New Card")
     |> assign(:card, %Cards.Card{})
-    |> assign(:project, Projects.get_project!(project_id))
+    |> assign(:column_id, column_id)
+    |> assign_project(project_id)
   end
 
-  defp apply_action(socket, :edit_card, %{"id" => project_id, "card_id" => card_id}) do
+  defp apply_action(socket, :edit_card, %{
+         "id" => project_id,
+         "column_id" => column_id,
+         "card_id" => card_id
+       }) do
     socket
     |> assign(:page_title, "Edit Card")
     |> assign(:card, Cards.get_card!(card_id))
-    |> assign(:project, Projects.get_project!(project_id))
+    |> assign(:column_id, column_id)
+    |> assign_project(project_id)
+  end
+
+  defp assign_project(socket, id) do
+    project = Projects.get_project!(id)
+
+    socket
+    |> assign(:project, project)
+    |> assign(:columns, project.columns)
+    |> assign(:cards, Enum.map(project.columns, fn col -> {col.id, col.cards} end))
   end
 
   @impl true
   def handle_info({CardzWeb.Components.Project.FormComponent, {:saved, _}}, socket) do
-    {:noreply, socket}
-  end
-
-  @impl true
-  def handle_info({CardzWeb.Components.Column.FormComponent, {:saved_column, _}}, socket) do
-    {:noreply, socket}
-  end
-
-  @impl true
-  def handle_info({CardzWeb.Components.Card.FormComponent, {:saved_card, _}}, socket) do
     {:noreply, socket}
   end
 
