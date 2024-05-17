2b6ed9f02f184e452ae302bf9502748062e3c36d
Fergus Molloy <fergus@molloy.xyz>
2024-05-18 00:25:43 +0100

    add seed data and create seed alias

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,54 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+project =
+  Cardz.Repo.insert!(%Cardz.Projects.Project{
+    title: "Example Project",
+    description: "a starter project"
+  })
+
+col_todo =
+  Cardz.Repo.insert!(%Cardz.Columns.Column{
+    title: "Todo",
+    description: "Tasks still to do",
+    project_id: project.id
+  })
+
+col_in_progress =
+  Cardz.Repo.insert!(%Cardz.Columns.Column{
+    title: "In Progress",
+    description: "Currently being worked on",
+    project_id: project.id
+  })
+
+col_done =
+  Cardz.Repo.insert!(%Cardz.Columns.Column{
+    title: "Done",
+    description: "Completed tasks",
+    project_id: project.id
+  })
+
+Cardz.Repo.insert!(%Cardz.Cards.Card{
+  title: "Add new card",
+  description: "add a new card to a column",
+  column_id: col_todo.id
+})
+
+Cardz.Repo.insert!(%Cardz.Cards.Card{
+  title: "Add new column",
+  description: "add a new column to a project",
+  column_id: col_todo.id
+})
+
+Cardz.Repo.insert!(%Cardz.Cards.Card{
+  title: "Explore the cardz app",
+  description: "take a look at the feature available",
+  column_id: col_in_progress.id
+})
+
+Cardz.Repo.insert!(%Cardz.Cards.Card{
+  title: "Successfully run the cardz app",
+  description: "get the app up and running and seed it with example data",
+  column_id: col_done.id
+})
